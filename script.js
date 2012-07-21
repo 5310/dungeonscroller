1baa5ed9c579fd513d7084f2d8ffcd00fefba358
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2012-07-21 21:03:39 +0530

    Basic init'ing for Crafty.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+window.onload = function () {
+	
+	// Initialize.
+    init();
+    
+};
 
+init = function() {
+	
+	// Dimensions.
+	var width = 1000;
+	var height = 1000;
+	
+	// Initialize Crafty.
+    Crafty.init(width, height);
+    Crafty.canvas.init();
+    
+};
