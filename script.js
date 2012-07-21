409fe8cdc4691ed1d847d41adbecb4f4f53c1c69
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2012-07-21 23:17:40 +0530

    Fixed sudden indentation issues. Geany bugs. How annoying.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 window.onload = function () {
 	
-	// Initialize.
+    // Initialize.
     init();
     
 };
 
 init = function() {
 	
-	// Dimensions.
-	var width = 1000;
-	var height = 1000;
-	
-	// Initialize Crafty.
+    // Dimensions.
+    var width = 1000;
+    var height = 1000;
+    
+    // Initialize Crafty.
     Crafty.init(width, height);
     Crafty.canvas.init();
     
@@ -20,9 +20,9 @@ init = function() {
     
     // Defining sprites.
     Crafty.sprite(24, "static.png", {
-		sprite_adventurer: [5, 2],
-		sprite_bat: [5, 14],
-		sprite_brick: [1, 39]
-	});
+	sprite_adventurer: [5, 2],
+	sprite_bat: [5, 14],
+	sprite_brick: [1, 39]
+    });
     
 };
