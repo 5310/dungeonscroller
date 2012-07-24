5b7d75865f09c4efd0bf738826279056cc33a2bd
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2012-07-24 11:31:19 +0530

    Added move function to initiate directional movement.

--- script.js
+++ script.js
@@ -49,48 +49,86 @@ init = function() {
 		// Shorthand for internal storage and other variables.
 		var move = this._move;
 		var unit = 24;
+		
+		// Regulate speed.						//TODO: This will be changed to allow easing.
 		move.speed = this.speed;
 		
-		// Calculate direction of movement.
-		var dx = move.mx != 0 ? move.mx > 0 ? 1 : -1 : 0;
-		var dy = move.my != 0 ? move.my > 0 ? 1 : -1 : 0;
-		
-		if ( move.state == 0 ) { // If entity ready to move:		//TODO: Convert this to a switch.
-		    
-		    // If there is movement, change state and set positions.
-		    if ( dx != 0 || dy != 0 ) {
-			move.state = 1;
-			move.nx = this.x;
-			move.ny = this.y;
-			move.ox = this.x + dx*unit;
-			move.oy = this.y + dy*unit;
-		    }
-		    
-		} 
-		if ( move.state == 1 ) {	// If entity is already moving.
+		if ( move.state == 1 ) {	// If entity is moving:
 
 		    // Move entity.
-		    this.x += dx*move.speed;
-		    this.y += dy*move.speed;
+		    this.x += move.mx*move.speed;
+		    this.y += move.my*move.speed;
 
 		    // Snap entity to target if reached.
-		    if ( (dx > 0 && this.x >= move.ox) || (dx < 0 && this.x <= move.ox) )
+		    if ( (move.mx > 0 && this.x >= move.ox) || (move.mx < 0 && this.x <= move.ox) )
 			this.x = move.ox;
-		    if ( (dy > 0 && this.y >= move.oy) || (dy < 0 && this.y <= move.oy) )
+		    if ( (move.my > 0 && this.y >= move.oy) || (move.my < 0 && this.y <= move.oy) )
 			this.y = move.oy;
 		    
 		    // If target reached, change state back to 0.
 		    if ( this.x == move.ox && this.y == move.oy ) {
 			move.state = 0;
-			move.mx -= dx;
-			move.my -= dy;
 		    }
 		    
 		}
 		
 	    }
 	},
-	speed_now: 0,		// Storage for current speed.
+	move: function( direction ) {	// Function to initiate movement.
+	    
+	    // direction:
+	    //	 1 = right
+	    //	 2 = down
+	    //	 3 = left
+	    //   4 = up
+	    
+	    // Shorthand and some variables.
+	    var move = this._move;
+	    var unit = 24;
+	    
+	    if ( move.state == 0 ) { // If entity ready to move.
+
+		// Set this._movement direction and sprite orientation.
+		switch ( direction ) {
+		    case 1:	// right
+			this._move.mx = 1;
+			this._move.my = 0;
+			this.unflip('X');
+			break;
+		    case 2:	// down
+			this._move.mx = 0;
+			this._move.my = 1;
+			break;
+		    case 3:	// left
+			this._move.mx = -1;
+			this._move.my = 0;
+			this.flip('X');
+			break;
+		    case 4:	// up
+			this._move.mx = 0;
+			this._move.my = -1;
+			break;
+		    default:	// cancel
+			return false;
+			break;
+		}
+				
+		// Set state.
+		this._move.state = 1;
+		// Set current discrete position.
+		this._move.nx = this.x;
+		this._move.ny = this.y;
+		// Set target discrete position.
+		this._move.ox = this.x + this._move.mx*unit;
+		this._move.oy = this.y + this._move.my*unit;
+		
+		return true;
+	    
+	    } else {
+		return false;
+	    }
+	    
+	},
 	init: function() {	// Initialization function for component.
 	    this.bind("EnterFrame", this._move.tick);
 	}
